b0ca78cde4b4265b996894eeabed49cb00a3824b
eguitarz <dalema22@gmail.com>
2012-03-17 18:43:17 +0800

    Remove test-read.rb

--- test-read.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'readability'
-require 'open-uri'
-
-source = open('http://graphics.stanford.edu/~mdfisher/projects.html').read
-puts Readability::Document.new(source).content
