ea01f6027c305ec7648854a9a0db13d563037e66
Mose <mose@mose.com>
2012-03-17 21:29:08 +0800

    fixed sample datas, new task is rake db:sample

--- db/sample.rb
+++
@@ -1,3 +0,0 @@
-# sample datas
-
-gather = Gather.create( [ :user_id => nil, :title => 'Test page', :url => 'http://google.com' ])
---
+++ db/samples.rb
@@ -0,0 +1,5 @@
+# sample datas
+
+Gather.delete_all
+Gather.create( [ :user_id => nil, :title => 'Test page', :url => 'http://google.com' ])
+Gather.create( [ :user_id => nil, :title => 'Test page 2', :url => 'http://ruby.tw' ])
