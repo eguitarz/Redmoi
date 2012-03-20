733214c35fcc9cc7eb358cbd9ffa7fdf172caaf3
Mose-blue <mose@codegreenit.com>
2012-03-20 20:42:51 +0800

    defining some accessors for models

--- app/models/format.rb
+++ app/models/format.rb
@@ -1,2 +1,4 @@
 class Format < ActiveRecord::Base
+
+  attr_accessible :name, :parser
 end
--- app/models/gather.rb
+++ app/models/gather.rb
@@ -1,4 +1,7 @@
 class Gather < ActiveRecord::Base
   has_one :format
+  belongs_to :user
+
+  attr_accessible :title, :content, :description, :keyword, :url
 
 end
