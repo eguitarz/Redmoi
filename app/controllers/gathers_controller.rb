c9698469addb72e02653cfeaf441bfa54d02bb49
eguitarz <dalema22@gmail.com>
2012-03-23 10:07:39 +0800

    Implements new and delete method, Redmoi is functioning.

--- app/controllers/gathers_controller.rb
+++ app/controllers/gathers_controller.rb
@@ -1,42 +1,57 @@
 class GathersController < ApplicationController
-  
+
   before_filter :get_gather, only: [:show, :edit, :destroy]
-  
+  before_filter :get_gathers, only: [:index, :menu, :show, :new]
+
   def index
-    @gathers = Gather.all
+
   end
-  
+
   def menu
-    @gathers = Gather.all
+
   end
-  
+
   def show
-    @gathers = Gather.all # to display menu
+    # Still need @gathers here to display menu
   end
-  
+
   def new
     @gather = Gather.new
   end
-  
+
   def create
-    
+    @gather = Gather.new(params[:gather])
+    text = open(@gather.url).read
+    @gather.title = Readability::Document.new(text).title
+    @gather.content = Readability::Document.new(text,
+      :tags => ['h1', 'h2', 'h3', 'img', 'li', 'ul', 'a', 'p', 'div', 'span'],
+      :attributes => ['src', 'href']).content
+
+    @gather.save
+
+    redirect_to gathers_url
   end
-  
+
   def edit
 
   end
-  
-  def update
-    
-  end
-  
-  def destroy
 
+  def update
+
+  end
+
+  def destroy
+    @gather.delete
+    redirect_to gathers_url
   end
 
   private
   def get_gather
     @gather = Gather.find(params[:id])
+  end
+
+  def get_gathers
+    @gathers = Gather.all
   end
 
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,11 @@ require 'open-uri'
 class WelcomeController < ApplicationController
   def index
     @gathers = Gather.all
-    
+
     text = open(@gathers.first.url).read
     @gather_title = Readability::Document.new(text).title
-    @gather_content = Readability::Document.new(text, 
-      :tags => ['h1', 'h2', 'h3', 'li', 'ul', 'a', 'p', 'div'],
-      :attributes => ['href']).content
+    @gather_content = Readability::Document.new(text,
+      :tags => ['img', 'h1', 'h2', 'h3', 'li', 'ul', 'a', 'p', 'div'],
+      :attributes => ['src', 'href']).content
   end
 end
