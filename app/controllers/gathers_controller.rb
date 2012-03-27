42063a753cff2ceb22f397fc5121bb239fb3d543
eguitarz <dalema22@gmail.com>
2012-03-27 14:41:55 +0800

    update: Assign user when creating new gather.

--- app/controllers/gathers_controller.rb
+++ app/controllers/gathers_controller.rb
@@ -2,6 +2,7 @@ class GathersController < ApplicationController
 
   before_filter :get_gather, only: [:show, :edit, :destroy]
   before_filter :get_gathers, only: [:index, :menu, :show, :new]
+  before_filter :authenticate_user!
 
   def index
 
@@ -27,6 +28,7 @@ class GathersController < ApplicationController
       :tags => ['h1', 'h2', 'h3', 'img', 'li', 'ul', 'a', 'p', 'div', 'span'],
       :attributes => ['src', 'href']).content
 
+    @gather.user = current_user
     @gather.save
 
     redirect_to gathers_url
