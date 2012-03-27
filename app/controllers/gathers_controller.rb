bc73a4b0724d5c5a79c280fb53ed7550af1e9359
eguitarz <dalema22@gmail.com>
2012-03-27 14:45:02 +0800

    update: Add comment

--- app/controllers/gathers_controller.rb
+++ app/controllers/gathers_controller.rb
@@ -3,6 +3,9 @@ class GathersController < ApplicationController
   before_filter :get_gather, only: [:show, :edit, :destroy]
   before_filter :get_gathers, only: [:index, :menu, :show, :new]
 
+  # Gathers Controller require verified users
+  before_filter :authenticate_user!
+
   def index
 
   end
@@ -27,6 +30,7 @@ class GathersController < ApplicationController
       :tags => ['h1', 'h2', 'h3', 'img', 'li', 'ul', 'a', 'p', 'div', 'span'],
       :attributes => ['src', 'href']).content
 
+    @gather.user = current_user
     @gather.save
 
     redirect_to gathers_url
