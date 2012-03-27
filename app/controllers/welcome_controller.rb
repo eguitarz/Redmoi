b19725fca9866d91c1a794c1cb86dfa46b4464cc
eguitarz <dalema22@gmail.com>
2012-03-28 00:10:56 +0800

    fix: fix exception in welcome page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,5 @@ require 'open-uri'
 class WelcomeController < ApplicationController
   def index
     @gathers = Gather.all
-
-    text = open(@gathers.first.url).read
-    @gather_title = Readability::Document.new(text).title
-    @gather_content = Readability::Document.new(text,
-      :tags => ['img', 'h1', 'h2', 'h3', 'li', 'ul', 'a', 'p', 'div'],
-      :attributes => ['src', 'href']).content
   end
 end
