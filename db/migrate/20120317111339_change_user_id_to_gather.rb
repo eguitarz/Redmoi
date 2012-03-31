d44aaebdc4bd85a157c53ef0cf19110fa9d57bd5
eguitarz <dalema22@gmail.com>
2012-03-31 23:44:58 +0800

    update: Change DB migration.

--- db/migrate/20120317111339_change_user_id_to_gather.rb
+++
@@ -1,9 +0,0 @@
-class ChangeUserIdToGather < ActiveRecord::Migration
-  def up
-    change_column :gathers, :user_id, :integer
-  end
-
-  def down
-    change_column :gathers, :user_id, :string
-  end
-end
