4aaa70694672350fdeb7a67aa94a6bc1b713203e
eguitarz <dalema22@gmail.com>
2012-04-04 23:13:46 +0800

    update: Update production settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Redmoi::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = { :host => 'www.redmoi.com' }
   require 'tlsmail'
   Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
   ActionMailer::Base.delivery_method = :smtp
