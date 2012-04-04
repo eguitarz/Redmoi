b6d441ee8328cf118f53d6fc2c92c1e44003631a
eguitarz <dalema22@gmail.com>
2012-04-04 23:08:01 +0800

    update: Update production settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Redmoi::Application.configure do
   Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.perform_deliveries = true
-  ActionMailer::Base.raise_delivery_errors = false
+  ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
     :enable_starttls_auto => true,
     :address            => 'smtp.gmail.com',
