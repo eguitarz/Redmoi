6e662a8dc6fa5da5babd8148c70ca6305bbee7c3
eguitarz <dalema22@gmail.com>
2012-04-04 22:58:09 +0800

    fix: Fix production.rb settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,18 +65,18 @@ Redmoi::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   require 'tlsmail'
-  net::smtp.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-  actionmailer::base.delivery_method = :smtp
-  actionmailer::base.perform_deliveries = true
-  actionmailer::base.raise_delivery_errors = true
-  actionmailer::base.smtp_settings = {
+  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = false
+  ActionMailer::Base.smtp_settings = {
     :enable_starttls_auto => true,
     :address            => 'smtp.gmail.com',
     :port               => 587,
     :tls                  => true,
     :domain             => 'redmoi.heroku.com',
     :authentication     => :plain,
-    :user_name          => env['GMAIL_USER_ID'],
-    :password           => env['GMAIL_USER_PASSWORD']
+    :user_name          => ENV['GMAIL_USER_ID'],
+    :password           => ENV['GMAIL_USER_PASSWORD']
   }
 end
