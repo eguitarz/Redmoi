349e351576e234174f47f41119d0b21b41ba7039
mose <mose@mose.tw>
2012-04-06 22:25:14 +0800

    working towards compliance with best practices

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :gathers
-  
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
