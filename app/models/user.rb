47e3fabe3d7db6c5c6f061d2646a5e781b61238b
eguitarz <dalema22@gmail.com>
2012-04-24 15:22:01 +0800

    update: Update juice.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,19 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :login, :username, :email, :password, :password_confirmation, :remember_me, :admin, :authentication_keys => [:login]
+  attr_accessible :login, :username, :email, :password, :password_confirmation, :remember_me, :admin
 
   # Virtual attribute for authenticating by either username or email
   # This is in addition to a real persisted field like 'username'
   attr_accessor :login
 
-  def self.find_for_database_authentication(warden_conditions)
-   conditions = warden_conditions.dup
-   login = conditions.delete(:login)
-   where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.strip.downcase }]).first
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login).downcase
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
   end
 
 end
