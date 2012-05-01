a3d7bf219d91603c68d7e7a60d3407586d5b0eab
eguitarz <dalema22@gmail.com>
2012-05-02 00:34:36 +0800

    update: The settings in initializers.

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,4 +1,4 @@
 # Default setting for general scope
-Settings.cache = ActiveSupport::Cache::MemoryStore.new
-Settings.cache_options = { :expires_in => 5.minutes }
-Settings.article_limit = 50
+Settings.defaults[:cache] = ActiveSupport::Cache::MemoryStore.new
+Settings.defaults[:cache_options] = { :expires_in => 5.minutes }
+Settings.defaults[:article_limit] = 50
